b4812fdf720ef2545ed926231445e4b6ef4301e6
Nafil Yasir <Nafilyasir1516@hotmail.com>
2024-10-24 11:09:12 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import SearchBar from './components/SearchBar';
+import MovieList from './components/MovieList';
 
 function App() {
-  return (
-    <div className="App">
-            <h1>Movie App</h1>
-        </div>
+  const [movies, setMovies] = useState([]);
+    const [searchQuery, setSearchQuery] = useState('Batman');
+
+    useEffect(() => {
+        const fetchMovies = async () => {
+            const response = await fetch(`https://www.omdbapi.com/?s=${searchQuery}&apikey=10d15504`);
+            const data = await response.json();
+            if (data.Search) {
+                setMovies(data.Search);
+            }
+        };
+
+        fetchMovies();
+    }, [searchQuery]);
+    return (
+      <div className="App">
+          <h1>Movie App</h1>
+          <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+          <MovieList movies={movies} />
+      </div>
   );
 }
 
